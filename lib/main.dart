b12ce45ff4bcee52fd2b7ebff3ecf347bbdfc54a
Edward Ho <pythoned77@gmail.com>
2022-08-18 22:32:25 +0800

    changed body to 3 questions

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favortie animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(child: Text('Answer 2'), onPressed: null),
+            ElevatedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
