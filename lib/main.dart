ee41ce891ebf8cdcb51c08bbdb695b747a954208
Edward Ho <pythoned77@gmail.com>
2022-08-20 13:56:33 +0800

    added score resultText and centralised it

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,11 @@ class _MyAppState extends State<MyApp> {
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -68,7 +71,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
                 questions: _questions)
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
